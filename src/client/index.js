64bcd2aac8bb619cec0fd6bed03f598a041bb055
Animesh <animathur@deloitte.com>
2019-05-24 14:46:46 +0530

    initial commit

---
+++ src/client/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/App';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+ReactDOM.hydrate(<App />, document.getElementById('app'));
+
+module.hot.accept();
---
+++ src/client/js/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+var quotes = [
+    ["Stay Hungry. Stay Foolish.", "Steve Jobs"],
+    ["Good Artists Copy, Great Artists Steal.", "Pablo Picasso"],
+    ["Argue with idiots, and you become an idiot.", "Paul Graham"],
+    ["Be yourself; everyone else is already taken.", "Oscar Wilde"],
+    ["Simplicity is the ultimate sophistication.", "Leonardo Da Vinci"]
+  ]
+const App = () => (
+    <div>
+        <h1>SSR is Awesome!</h1>
+        {quotes.map(quote => (<p key={quote[1]}>
+            {quote[0]} - <i>{quote[1]}</i>
+        </p>))}
+    </div>
+);
+
+export default App;
